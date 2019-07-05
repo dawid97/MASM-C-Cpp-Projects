457bfc1a95f6fc1d5caacaca087ccdadbbd10cd8
dawid97 <32642357+dawid97@users.noreply.github.com>
2019-07-05 18:19:19 +0200

    Projekt

---
+++ ProjectDawid/src/com/Dawid/Main.java
@@ -0,0 +1,11 @@
+package com.Dawid;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mathematics mat = new Mathematics();
+        System.out.println(mat.power(6));
+        System.out.println(mat.power(6.56));
+    }
+}
+
---
+++ ProjectDawid/src/com/Dawid/Mathematics.java
@@ -0,0 +1,20 @@
+package com.Dawid;
+
+public class Mathematics {
+
+    public int power(int a){
+        return a*a;
+    }
+
+     public double power(double a){
+       return a*a;
+     }
+}
+
+
+
+
+
+
+
+
